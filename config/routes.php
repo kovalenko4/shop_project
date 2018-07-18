2cc957e4b8be3f87976a99da7e7f6006a381786b
Pavlo Kovalenko <kovalenkogitacc@gmail.com>
2018-07-18 17:43:01 +0300

    second commit, array

--- config/routes.php
+++ config/routes.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Pashulya
- * Date: 17.07.2018
- * Time: 9:09
- */
+
     return array(
-        'news/([0-9]+)' => 'news/view',
-
         'news' => 'news/index',
-
+        'products' => 'product/list',
     );
